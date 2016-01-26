2e1197a95a12a3861e2f220fb2e6aa3ea9b7ece9
Adrian Rossouw <adrian@daemon.co.za>
2016-01-26 02:52:55 +0200

    remove dead files

--- examples/blog/entity.js
+++
@@ -1,8 +0,0 @@
-var queries = require('./queries');
-var store = require('../../massive-store');
-
-module.exports = function(opts) {
-	var seneca = this;
-
-	seneca.use(store('-/-/blog', queries));
-};
